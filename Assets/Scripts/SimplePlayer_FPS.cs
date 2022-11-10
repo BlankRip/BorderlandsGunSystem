7873ecbcd8336d3d7a26f5b407836d0cf9a55a2d
BlankRip <aniketh_g5@outlook.com>
2022-11-10 13:30:07 +0530

    Player movement coded in

--- Assets/Scripts/SimplePlayer_FPS.cs
+++ Assets/Scripts/SimplePlayer_FPS.cs
@@ -5,6 +5,56 @@ using UnityEngine;
 namespace Gameplay.Player {
     public class SimplePlayer_FPS : MonoBehaviour
     {
-        
+        [SerializeField] CharacterController cc;
+        [SerializeField] float movementSpeed = 10;
+        [SerializeField] float gravity = -19.2f;
+        [SerializeField] float jumpHight = 5;
+
+        private Vector3 gravityVector;
+        private float horizontalInput, verticalInput;
+        private bool jump;
+        private bool grounded;
+
+        private void Start() {
+            if(cc == null) {
+                cc = GetComponent<CharacterController>();
+                if(cc == null)
+                    cc = GetComponentInChildren<CharacterController>();
+            }
+            
+            if(gravity > 0)
+                gravity *= -1;
+        }
+
+        private void Update() {
+            grounded = cc.isGrounded;
+            UpdateInputData();
+            Move();
+        }
+
+        private void UpdateInputData() {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+            if(Input.GetKeyDown(KeyCode.Space))
+                jump = true;
+        }
+
+        private void Move() {
+            if (grounded && gravityVector.y < 0)
+                gravityVector.y = -2;
+            Vector3 moveDir = ((transform.forward * verticalInput) + (transform.right * horizontalInput)).normalized;
+            Vector3 move = moveDir * movementSpeed * Time.deltaTime;
+            cc.Move(move);
+
+            if (jump) {
+                if(grounded)
+                    gravityVector.y = Mathf.Sqrt(jumpHight * -2 * gravity);
+                else
+                    jump = false;
+            }
+            
+            gravityVector.y += gravity * Time.deltaTime;
+            cc.Move(gravityVector * Time.deltaTime);
+        }
     }
 }
